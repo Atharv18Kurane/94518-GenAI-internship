34e65d2744762e1c3d57e14a31a0546504712efc
Atharv Kurane <atharvakurane1810@gmail.com>
2025-12-11 12:52:22 +0530

    ass1

--- Assignment1/Q1.py
+++ Assignment1/Q1.py
@@ -1,1 +1,27 @@
+"""Q1:
+Write a Python program that takes a sentence from the user and prints:
 
+Number of characters
+
+Number of words
+
+Number of vowels
+
+Hint: Use split(), loops, and vowel checking."""
+
+s1=input("enter a sentence")
+
+char = len(s1)
+
+words=len(s1.split())
+
+vowel="AEIOUaeiou"
+count=0
+
+for ch in s1:
+    if ch in vowel:
+        count=count+1
+
+print("characters:", char)
+print("words:" ,words)
+print("vowels are:", count)
--- Assignment1/Q2.py
+++ Assignment1/Q2.py
@@ -1,1 +1,31 @@
+"""Q2:
+
+Count Even and Odd Numbers
+
+Take a list of numbers as input (comma-separated).
+
+Count how many are even and how many are odd.
+
+Print results.
+
+Example Input:
+10, 21, 4, 7, 8"""
+
+num=input("Enter numbers (comma-separated):")
+num_list = [int(num)for num in num.split(",")]
+
+even_count=0
+odd_count=0
+
+for num in num_list:
+    if num%2==0:
+        even_count+=1
+    else:
+        odd_count+=1
+
+print("Even count is:",even_count)
+print("Odd count is :", odd_count)
+
+
+
 
