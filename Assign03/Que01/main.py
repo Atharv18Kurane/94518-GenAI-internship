f0ff7e10dc0a3e4a25175201b3e569e3c161d12d
Atharv Kurane <atharvakurane1810@gmail.com>
2025-12-14 20:32:26 +0530

    sqlonpandas

--- Assign03/Que01/main.py
+++ Assign03/Que01/main.py
@@ -1,1 +1,18 @@
+"""1.
+Upload a CSV file. Input a SQL query from user and execute it on the CSV
+data (as dataframe ). Display result on the"""
 
+import pandas as pd 
+import pandasql as ps 
+
+filepath="emp_hdr.csv"
+df=pd.read_csv(filepath)
+print("Dataframe Column Types:")
+print(df.dtypes)
+print("\nEmp Data:")
+print(df)
+
+query= "SELECT job,SUM(sal) total FROM data GROUP BY job"
+result=ps.sqldf(query,{"data":df})
+print("\nQuery result:")
+print(result)
