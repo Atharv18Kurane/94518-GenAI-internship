0a10ef0520f3253925e12dbd128512e3dcc8de03
Atharv Kurane <atharvakurane1810@gmail.com>
2025-12-16 18:55:47 +0530

    Assign4

--- Day03/Selenium/practice.py
+++ Day03/Selenium/practice.py
@@ -1,1 +1,16 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
 
+chrome_options=Options()
+chrome_options.add_argument("--headless")
+driver=webdriver.Chrome(options=chrome_options)
+
+driver.get("https://sunbeaminfo.in/")
+print("Page Title:",driver.title)
+
+driver.implicitly_wait(5)
+
+intern=driver.find_element(By.ID,"INTERNSHIP").click()
+available_internship_programs=driver.find_elements(By.XPATH,"https://sunbeaminfo.in/#collapseSix")
+available_internship_programs=driver.find_elements(By.XPATH,"//*[@id=collapseSix]/div/div/div[2]")
