d2f8fd3add2f30c1fadd364139d4a5d23ba1c2dd
Atharv Kurane <atharvakurane1810@gmail.com>
2025-12-22 11:44:59 +0530

    day5 all langchain demos

--- Day05/Langchain/demo01.py
+++ Day05/Langchain/demo01.py
@@ -1,1 +1,22 @@
+from langchain_groq import ChatGroq
+from langchain_openai import ChatOpenAI
+import os
+import dotenv
+dotenv.load_dotenv()
 
+api_key=os.getenv("GROQ_API_KEY")
+llm = ChatGroq(model="openai/gpt-oss-120b", api_key=api_key)
+
+# llm_url="http://127.0.0.1:1234/v1"
+# llm=ChatOpenAI(
+#     base_url=llm_url,
+#     model="meta-llama-3.1-8b-instruct",
+#     api_key="dummy_api_key"
+# )
+
+user_input=input("Ask anything")
+# result = llm.invoke(user_input)
+# print("AI: ", result.content)
+result = llm.stream(user_input)
+for chunk in result:
+    print(chunk.content, end="")
--- Day05/Langchain/demo03.py
+++ Day05/Langchain/demo03.py
@@ -1,1 +1,29 @@
+from langchain.chat_models import init_chat_model
+import os 
+from dotenv import load_dotenv
+
+load_dotenv()
+llm=init_chat_model(
+    model= "llama-3.3-70b-versatile",
+    model_provider="openai",
+    base_url="https://api.groq.com/openai/v1",
+    api_key=os.getenv("GROQ_API_KEY")
+)
+k=0
+input("enter k value:",k)
+conversation= list()
+while True:
+    user_input=input("You:")
+    if user_input==exit:
+        break
+    user_msg = {"role":"user", "content":user_input}
+    conversation.append(user_msg)
+    system_msg = conversation[0]
+    recent_msgs = conversation[-k * 2:]   # user + assistant pairs
+    context = [system_msg] + recent_msgs
+    llm_output = llm.invoke(conversation)
+    print("AI:",llm_output.content)
+    llm_msg = {"role":"assistant","content":llm_output.content}
+    conversation.append(llm_msg)
+
 
