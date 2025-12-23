d875b3694303cbf1f7c1ee5a827309a85ef7ca20
Atharv Kurane <atharvakurane1810@gmail.com>
2025-12-23 19:00:34 +0530

    embedding demo

--- Day07/RAG/demo01.py
+++ Day07/RAG/demo01.py
@@ -1,1 +1,21 @@
+from sentence_transformers import SentenceTransformer
+import numpy as np 
+
+def cosine_similarity(a,b):
+    return np.dot(a,b) / (np.linalg.norm(a) * np.linalg.norm(b))
+
+embedded_model = SentenceTransformer("all-MiniLM-L6-v2")
+sentenses = [
+    "I love Cricket.",
+    "Running is my favorite sport.",
+    "Messi talks spanish."
+]
+
+emebeddings = embedded_model.encode(sentenses)
+
+for embedded_vect in emebeddings:
+    print("Length:", len(embedded_vect), "-->",embedded_vect[:4])
+
+print("Sentence 1 & 2 similarity:", cosine_similarity(emebeddings[0],emebeddings[1]))
+print("Sentence 1 & 3 similarity:", cosine_similarity(emebeddings[0],emebeddings[2]))
 
--- Day07/RAG/demo02.py
+++ Day07/RAG/demo02.py
@@ -1,1 +1,27 @@
+from langchain_openai import OpenAIEmbeddings
+import numpy as np 
+
+def cosine_similarity(a,b):
+    return np.dot(a,b) / (np.linalg.norm(a) * np.linalg.norm(b))
+
+embedded_model = OpenAIEmbeddings(
+    model="text-embedding-nomic-embed-text-v1.5",
+    base_url="http://127.0.0.1:1234/v1",
+    api_key="dummy_token",
+    check_embedding_ctx_length=False
+)
+sentenses = [
+    "I love Cricket.",
+    "Running is my favorite sport.",
+    "Messi talks spanish."
+]
+
+emebeddings = embedded_model.embed_documents(sentenses)
+
+for embedded_vect in emebeddings:
+    print("Length:", len(embedded_vect), "-->",embedded_vect[:4])
+
+print("Sentence 1 & 2 similarity:", cosine_similarity(emebeddings[0],emebeddings[1]))
+print("Sentence 1 & 3 similarity:", cosine_similarity(emebeddings[0],emebeddings[2]))
+
 
